06ca1cb756cf3b61766c9f8e0ee33c35a5ccb0db
nghiavt3 <nghiavt3@vnpay.vn>
2026-01-22 16:49:22 +0700

    đẩy sepolia

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,19 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config(); // Nạp biến môi trường
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const SEPOLIA_URL = process.env.SEPOLIA_URL || "";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
+
 module.exports = {
-  solidity: "0.8.28",
+  solidity: "0.8.0",
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545"
+    },
+    sepolia: {
+      url: SEPOLIA_URL,
+      accounts: [PRIVATE_KEY]
+    }
+  }
 };
